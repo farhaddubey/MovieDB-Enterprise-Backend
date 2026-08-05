52475900f28124a6ed88462b119affc91d984bc0
farhaddubey <farhaddubey003@gmail.com>
2026-08-05 16:18:15 +0530

    updating

---
+++ src/main/java/com/backend/movie_db/MovieDB_Enterprise_Backend/MovieDbEnterpriseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.movie_db.MovieDB_Enterprise_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDbEnterpriseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDbEnterpriseBackendApplication.class, args);
+	}
+
+}
